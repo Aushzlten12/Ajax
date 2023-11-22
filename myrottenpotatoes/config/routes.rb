beff06ce1b3c1ba3da7bc99cfe5cc7320763ba24
Aushzlten12 <josepachas1604@gmail.com>
2023-11-22 09:48:40 -0500

    Avance en clase

--- myrottenpotatoes/config/routes.rb
+++ myrottenpotatoes/config/routes.rb
@@ -3,5 +3,8 @@ Myrottenpotatoes::Application.routes.draw do
   resources :movies do
   resources :reviews
 end
+  get 'auth/:provider/callback', to: 'sessions#create'
+  get '/login', to: 'sessions#new'
+  post 'logout' => 'sessions#destroy'
   root :to => redirect('/movies')
 end
