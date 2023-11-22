864d9794c1f0c9c7388eb80073e94b9543ede2ab
Aushzlten12 <josepachas1604@gmail.com>
2023-11-22 09:28:42 -0500

    Agregando migracion moviegoers

---
+++ myrottenpotatoes/db/migrate/20231122142551_moviegoer.rb
@@ -0,0 +1,10 @@
+class Moviegoer < ActiveRecord::Migration[7.0]
+  def change
+    create_table 'moviegoer' do |t|
+      t
+    
+      t.timestamps
+    end
+    
+  end
+end
--- myrottenpotatoes/db/schema.rb
+++ myrottenpotatoes/db/schema.rb
@@ -10,7 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_11_22_015741) do
+ActiveRecord::Schema[7.0].define(version: 2023_11_22_142726) do
+  create_table "moviegoer", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "moviegoers", force: :cascade do |t|
+    t.string "name"
+    t.string "provider"
+    t.string "uid"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "movies", force: :cascade do |t|
     t.string "title"
     t.string "rating"
